fafa2b3041ebfd981be22bc0a8831c5770e2363a
disek13 <disek@bk.ru>
2021-10-25 09:19:53 +1000

    The second task done

--- src/main/java/ru/gb/Homework1.java
+++ src/main/java/ru/gb/Homework1.java
@@ -3,11 +3,17 @@ package ru.gb;
 public class Homework1 {
     public static void main(String[] args) {
      printThreeWords();
+     checkSunSign();
     }
     public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+        System.out.println("Orange\nBanana\nApple");
+
+    }
+    public static void checkSunSign() {
+        int a = 13, b = 5;
+        int c = a + b;
+        final String s = (c >= 0 ? "положительная" : "отрицательная");
+        System.out.println("Сумма "+ s);
 
     }
 }
