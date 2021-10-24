5c50c3e0315401774f0b50c5d108bdff7516af18
disek13 <disek@bk.ru>
2021-10-25 09:27:56 +1000

    The 3 task done

--- src/main/java/ru/gb/Homework1.java
+++ src/main/java/ru/gb/Homework1.java
@@ -4,6 +4,8 @@ public class Homework1 {
     public static void main(String[] args) {
      printThreeWords();
      checkSunSign();
+     printColor();
+     compareNumbers();
     }
     public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
@@ -15,5 +17,24 @@ public class Homework1 {
         final String s = (c >= 0 ? "положительная" : "отрицательная");
         System.out.println("Сумма "+ s);
 
+    }
+    public static void printColor() {
+        int value = 5;
+        if (value <=0)  {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 8;
+        int b = 7;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 }
