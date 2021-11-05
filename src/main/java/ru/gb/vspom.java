ef2cd0a8d5f0e248233d17a310ba03a7b6807edf
disek13 <disek@bk.ru>
2021-11-05 19:27:23 +1000

    Игра быки и коровы Добавил возможность ввода букв в любом регистре

--- src/main/java/ru/gb/vspom.java
+++ src/main/java/ru/gb/vspom.java
@@ -4,22 +4,17 @@ import java.util.Scanner;
 
 class vspom {
     public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
-        //int s = sc.nextInt();
-        //s = s % 86400;
-        //int h = s / 3600;
-        //int m = s % 3600 / 60;
-       // int osts = s % 60;
-       // String myVar = String.valueOf(h / 10) + String.valueOf(h % 10) + ":" + String.valueOf(m / 10) + String.valueOf(m % 10) + ":" + String.valueOf(osts / 10) + String.valueOf(osts % 10);
-       // System.out.print(myVar);
-      //  int a = 5;
-        //boolean b;
-        //int c = 4;
-       // b = c++ == 4 && a++ < 6;
-        int x = 5, y = 3;
-        int z = ++x;
-        y += x--;
-        z+= --x + y++;
-        System.out.println(x + y + z);
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int summa = x + y;
+        int umn = x * y;
+        if (summa % 2 == 0 && (umn % 2 > 0)) {
+            System.out.print("true");
+
+        } else {
+            System.out.print("false");
+
+        }
     }
 }
